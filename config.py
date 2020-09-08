8088d931d0188cbd99bd3ceaeaa8e61b371a7f31
MYKingma <55245466+MYKingma@users.noreply.github.com>
2020-09-08 17:16:34 +0200

    test 7

--- config.py
+++ config.py
@@ -90,7 +90,7 @@ app.config['ALLOWED_EXTENSIONS'] = os.getenv("ALLOWED_EXTENSIONS")
 
 # set up GitHub for file upload
 g = Github(os.getenv("GITHUB_ACCESS_TOKEN"))
-repo = g.get_repo("MYKingma/tree")
+repo = g.get_repo("tree")
 
 def allowed_file(filename):
     return '.' in filename and \
