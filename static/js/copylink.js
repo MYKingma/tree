fd425cb5f69877464e0b45b91ad1173daba73e4b
MYKingma <55245466+MYKingma@users.noreply.github.com>
2020-09-08 19:39:30 +0200

    Update copylink.js

--- static/js/copylink.js
+++ static/js/copylink.js
@@ -8,7 +8,8 @@ Javascript for copying image link
 function newLink() {
     document.querySelectorAll('#sPath').forEach(link => {
         link.onclick = () => {
-            navigator.clipboard.writeText(link.dataset.link);
+            console.log(link.dataset.link);
+            // navigator.clipboard.writeText(link.dataset.link);
             link.childNodes[0].className = "fas fa-check"
             setTimeout(function(){
                 link.childNodes[0].className = "fas fa-copy"
